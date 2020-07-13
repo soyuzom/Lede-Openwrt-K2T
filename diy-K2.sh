151517afb0a64900a1636456291f8b4728e0eed1
SoyuZom <soyu@idoog.me>
2020-07-14 02:00:19 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -8,8 +8,15 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# 更改改机器名称
+# 更改主机名
 sed -i 's/OpenWrt/K2/g' package/base-files/files/bin/config_generate
+
+# 更改时区
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# 更改主题
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
