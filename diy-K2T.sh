dcfda1c319ed3c6edb0d406615ef703053f0e950
SoyuZom <soyu@idoog.me>
2020-07-25 11:21:58 +0800

    Update diy-K2T.sh

--- diy-K2T.sh
+++ diy-K2T.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#============================================================
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# File name: diy-K2T.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+#
 
 # 更改主机名
 sed -i 's/OpenWrt/K2T/g' package/base-files/files/bin/config_generate
