a9bea365219d19177f77fda8f4b00a0d976b7113
SoyuZom <soyu@idoog.me>
2021-01-05 18:34:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# 应用过滤OpenAppFilter 【luci oaf app】
+# 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
 cd package
 git clone https://github.com/destan19/OpenAppFilter.git
 cd -
