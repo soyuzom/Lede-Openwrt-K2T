67b275e85b3ea2e09bb01a92105157956dd36688
SoyuZom <soyu@idoog.me>
2021-01-05 18:14:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+cd package
 git clone https://github.com/destan19/OpenAppFilter.git
+cd -
