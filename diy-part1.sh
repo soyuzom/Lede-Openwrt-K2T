6e965d42c52e39b732c3c689193d7215d08d8c03
SoyuZom <soyu@idoog.me>
2021-04-22 12:19:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 应用过滤OpenAppFilter 【luci oaf app】
 # 和加速模块（Turbo ACC)有冲突，需要关闭Turboo ACC后使用
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
 
